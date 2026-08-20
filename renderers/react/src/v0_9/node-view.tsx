40dd8ece1274c3389020fc8a91cb1ddd4fc10083
Andrew Kolos <andrewrkolos@gmail.com>
2026-08-20 08:35:14 -0700

    fix(react): import ComponentNode as a type
    ComponentNode is an interface now; the explorer app compiles with
    verbatimModuleSyntax, which rejects value imports of types.

--- renderers/react/src/v0_9/node-view.tsx
+++ renderers/react/src/v0_9/node-view.tsx
@@ -1,11 +1,11 @@
-/**
- * Copyright 2026 Google LLC
+/*
+ * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,7 +18,7 @@ import type React from 'react';
 import {createContext, useCallback, useContext, useMemo, useSyncExternalStore} from 'react';
 import {
   ComponentContext,
-  ComponentNode,
+  type ComponentNode,
   isComponentNode,
   ResolvedBinding,
   isWritable,
