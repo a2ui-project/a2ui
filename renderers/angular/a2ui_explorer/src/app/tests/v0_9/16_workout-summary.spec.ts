fef171d767485b1dfe7f2d43089a2850830838b2
Jacob Simionato <jsimionato@google.com>
2026-09-19 01:47:45 +0000

    style: fix prettier formatting in angular explorer tests

--- renderers/angular/a2ui_explorer/src/app/tests/v0_9/16_workout-summary.spec.ts
+++ renderers/angular/a2ui_explorer/src/app/tests/v0_9/16_workout-summary.spec.ts
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-import { ComponentFixture } from '@angular/core/testing';
-import { DemoComponent } from '../../demo.component';
-import { getCanvas, loadExample } from '../utils';
+import {ComponentFixture} from '@angular/core/testing';
+import {DemoComponent} from '../../demo.component';
+import {getCanvas, loadExample} from '../utils';
 
 describe('Example: Workout Summary', () => {
   let fixture: ComponentFixture<DemoComponent>;
